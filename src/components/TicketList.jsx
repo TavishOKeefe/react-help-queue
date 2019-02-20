27053e2fa8adbac901f76b88697e1333c88e8f07
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-02-20 08:24:52 -0800

    add stuff.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -20,17 +20,17 @@ var masterTicketList = [
 ];
 
 function TicketList(){
-    return (
-      <div>
-        <hr/>
-        {masterTicketList.map((ticket, index) =>
-          <Ticket names={ticket.names}
-            location={ticket.location}
-            issue={ticket.issue}
-            key={index}/>
-        )}
-      </div>
-    );
+  return (
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index}/>
+      )}
+    </div>
+  );
 }
 
 export default TicketList;
